c3aec1962e3b743e18d4989fa613453d6d74ed3a
Oluwa-Laughter <ismakinde@gmail.com>
2025-04-11 17:35:16 +0100

    OOP rendering HTML elements

--- dist/app.js
+++ dist/app.js
@@ -1,2 +1,15 @@
 "use strict";
+class App {
+    constructor() {
+        this.templateEl = document.getElementById("project-input");
+        this.hostEl = document.getElementById("app");
+        const importedNode = document.importNode(this.templateEl.content, true);
+        this.element = importedNode.firstElementChild;
+        this.attach();
+    }
+    attach() {
+        this.hostEl.insertAdjacentElement("afterbegin", this.element);
+    }
+}
+const app = new App();
 //# sourceMappingURL=app.js.map
