73236c4b3a31a4b17c9cb5116a36869eb2b5bd7e
Oluwa-Laughter <ismakinde@gmail.com>
2025-04-11 17:49:29 +0100

    interacting with the DOM element

--- dist/app.js
+++ dist/app.js
@@ -5,7 +5,19 @@ class App {
         this.hostEl = document.getElementById("app");
         const importedNode = document.importNode(this.templateEl.content, true);
         this.element = importedNode.firstElementChild;
+        this.element.id = "user-input";
+        this.titleInputEl = this.element.querySelector("#title");
+        this.descriptionInputEl = this.element.querySelector("#description");
+        this.peopleInputEl = this.element.querySelector("#people");
+        this.config();
         this.attach();
+    }
+    submitHandler(e) {
+        e.preventDefault();
+        console.log(this);
+    }
+    config() {
+        this.element.addEventListener("submit", this.submitHandler.bind(this));
     }
     attach() {
         this.hostEl.insertAdjacentElement("afterbegin", this.element);
--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,9 @@ class App {
   templateEl: HTMLTemplateElement;
   hostEl: HTMLDivElement;
   element: HTMLFormElement;
+  titleInputEl: HTMLInputElement;
+  descriptionInputEl: HTMLInputElement;
+  peopleInputEl: HTMLInputElement;
 
   constructor() {
     this.templateEl = document.getElementById(
@@ -13,8 +16,32 @@ class App {
     const importedNode = document.importNode(this.templateEl.content, true);
 
     this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.element.id = "user-input";
+
+    this.titleInputEl = this.element.querySelector(
+      "#title"
+    )! as HTMLInputElement;
+
+    this.descriptionInputEl = this.element.querySelector(
+      "#description"
+    )! as HTMLInputElement;
+
+    this.peopleInputEl = this.element.querySelector(
+      "#people"
+    )! as HTMLInputElement;
+
+    this.config();
 
     this.attach();
+  }
+
+  private submitHandler(e: Event) {
+    e.preventDefault();
+    console.log(this);
+  }
+
+  private config() {
+    this.element.addEventListener("submit", this.submitHandler.bind(this));
   }
 
   private attach() {
